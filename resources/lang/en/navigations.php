bc909eb27bef7c10778a7f45cbc7d7e2d74dbefb
Kamaro Lambert <kamaroly@gmail.com>
2015-07-01 11:37:19 +0200

    Added fonts

---
+++ resources/lang/en/navigations.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Navigation Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used by the navigation to build
+	| the simple navigation links. You are free to change them to anything
+	| you want to customize your views to better match your application
+	|
+	 */
+
+	'dashboard' => 'Dashboard',
+	'members' => 'Members',
+	'contributions_and_savings' => 'Contributions & savings',
+	'loans_and_repayments' => 'Loans & repayments',
+
+];
