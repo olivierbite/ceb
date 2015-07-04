5716e4ceb2200334d75deea21a0de3fd222871c4
Kamaro Lambert <kamaroly@gmail.com>
2015-07-04 09:20:47 +0200

    Corrected page title at login

--- resources/views/sentinel/sessions/login.blade.php
+++ resources/views/sentinel/sessions/login.blade.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="UTF-8">
-    <title>PIASS Student</title>
+    <title>CEB MS</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
        <!-- Bootstrap 3.3.2 -->
     <link href="{{Url()}}/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
