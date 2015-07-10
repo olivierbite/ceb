96f2d4b8b641c747d56de6cff632ebbfc2f6272a
Kamaro Lambert <kamaroly@gmail.com>
2015-07-10 14:53:12 +0200

    Removed blank pages in the member page

--- resources/views/files/show.blade.php
+++ resources/views/files/show.blade.php
@@ -1,6 +1,5 @@
 @if(isset($filename) && !empty($filename))
-
-                    <img src="{{route('files.get', $filename)}}" alt="ALT NAME" class="img-responsive" />
+<img src="{{route('files.get', $filename)}}" alt="ALT NAME" class="img-responsive" />
 @else 
 	<i class="fa fa-picture-o" style="font-size:140px;"></i>
 @endif
