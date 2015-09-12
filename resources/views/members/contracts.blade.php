92222f902f6aa67f3fc444e1a06219c264f14b7d
Kamaro Lambert <kamaroly@gmail.com>
2015-09-12 12:14:54 +0200

    Only show contracts report buttons if they are available

--- resources/views/members/contracts.blade.php
+++ resources/views/members/contracts.blade.php
@@ -1,14 +1,19 @@
-<div class="containter row">
+@if(count($member->contributions) > 0)
+<div class="row">
 <a 	class="btn btn-success"
 	href="{{ route('reports.contracts.saving',['memberId' => $member->id]) }}"
 	target="_blank"
 >
 {{ trans('member.contract_saving') }}</a>
 </div>
+@endif
 
+@if(count($member->loans) > 0)
+<div class="row">
 <a 	class="btn btn-success"
 	href="{{ route('reports.contracts.loan',['memberId' => $member->id]) }}"
 	target="_blank"
 >
 {{ trans('member.contract_loan') }}</a>
 </div>
+@endif
