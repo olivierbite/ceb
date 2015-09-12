41ca8f3c0f6cdf16758b5332a9e272568be77d31
Kamaro Lambert <kamaroly@gmail.com>
2015-09-12 11:47:10 +0200

    Added attorney name

--- resources/views/attornies/item.blade.php
+++ resources/views/attornies/item.blade.php
@@ -1,6 +1,7 @@
 <div class="col-lg-12 col-sm-12">
     <div class="btn-pref btn-group btn-group-justified btn-group-lg" role="group" aria-label="...">
         <div class="btn-group" role="group">
+            <label>{!! $attorney->names !!}</label>
             @include('files.show',['filename'=>$attorney->photo])
         </div>
         <div class="btn-group" role="group">
