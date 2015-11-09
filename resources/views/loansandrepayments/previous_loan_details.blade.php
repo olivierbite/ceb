12c10f356153cadccb4387f8e3fbafa497003e9a
Kamaro Lambert <kamaroly@gmail.com>
2015-11-09 15:05:38 +0200

    Changed the form for the regularisation

--- resources/views/loansandrepayments/previous_loan_details.blade.php
+++ resources/views/loansandrepayments/previous_loan_details.blade.php
@@ -2,8 +2,8 @@
   <div class="col-md-3">
   <div class="form-group">
    <label>{{ trans('loan.previous_loan_contract_number') }}</label>
-    {!! Form::input('text', 'current_loan_contract',$member->loan_contract,
-                  ['class'=>'form-control'])
+    {!! Form::input('text', 'current_loan_contract',$member->latestLoan()->loan_contract,
+                  ['class'=>'form-control','disabled'=>true])
     !!}
   </div>
   </div>
@@ -11,31 +11,31 @@
   <div class="form-group">
    <label>{{ trans('loan.remaining_installments') }}</label>
     {!! Form::input('text', 'current_number_of_installments',$member->remaining_tranches,
-                  ['class'=>'form-control'])
-    !!}
-  </div>
-  </div>
-  <div class="col-md-3">
-  <div class="form-group">
-   <label>{{ trans('loan.current_loan_to_repay') }}</label>
-    {!! Form::input('text', 'current_loan_to_repay',$member->loan_to_repay,
-                  ['class'=>'form-control'])
+                  ['class'=>'form-control','disabled'=>true])
     !!}
   </div>
   </div>
   <div class="col-md-2">
   <div class="form-group">
    <label>{{ trans('loan.current_monthly_fees') }}</label>
-       {!! Form::input('text', 'current_monthly_fees',$member->monthly_fees,
-                  ['class'=>'form-control'])
+       {!! Form::input('text', 'current_monthly_fees',$member->latestLoan()->monthly_fees,
+                  ['class'=>'form-control','disabled'=>true])
     !!}
    </div>
   </div>
-    <div class="col-md-2">
+  <div class="col-md-2">
   <div class="form-group">
    <label>{{ trans('loan.previous_loan_balance') }}</label>
        {!! Form::input('text', 'current_monthly_fees',($member->exists) ?$member->loanBalance() : 0,
-                  ['class'=>'form-control'])
+                  ['class'=>'form-control','disabled'=>true])
+    !!}
+   </div>
+  </div>
+  <div class="col-md-2">
+  <div class="form-group">
+   <label>{{ trans('loan.previous_loan_type') }}</label>
+       {!! Form::input('text', 'current_monthly_fees',($member->exists) ?$member->latestLoan()->operation_type : 'n/a',
+                  ['class'=>'form-control','disabled'=>true])
     !!}
    </div>
   </div>
