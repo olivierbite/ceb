ba44441992674e274728821a9e63e7c1099c2442
Kamaro Lambert <kamaroly@gmail.com>
2015-11-12 11:25:16 +0200

    Removed copyright in the footer since this is copy righted to Lambert Kamaro

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,6 +2,5 @@
         <div class="pull-right hidden-xs">
           <b>{{ trans('footer.version') }}</b> 2.0
         </div>
-        <strong>{{ trans('footer.copy_right') }} &copy; 2015 <a href="http://huguka.com" target="_blank">{{ trans('footer.ceb') }} & Kamaro Lambert</a>.</strong>
-        {{ trans('footer.all_right_reserved') }}
+        <strong> <a href="http://huguka.com" target="_blank">{{ trans('footer.ceb') }} </a>.</strong>
 </footer>
