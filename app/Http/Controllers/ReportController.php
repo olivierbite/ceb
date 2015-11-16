a39884d8ab33063f31326bb27ffb61309365f1a5
Kamaro Lambert <kamaroly@gmail.com>
2015-11-16 20:42:28 +0200

    fixed bug of undefined member variable in report controller

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -56,7 +56,8 @@ class ReportController extends Controller {
 
         // First log 
         Log::info($this->user->email . ' is viewing report contract saving');
-
+    
+		$member = $this->getMember($memberId);
 
 		if ($member != false) {
 
