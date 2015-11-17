f3bb641e970d045db499d923c3ac47013fafb51e
Kamaro Lambert <kamaroly@gmail.com>
2015-11-18 00:38:17 +0200

    Added latest loan attribute to member model, and refactored previous loan view in regulation

--- resources/views/regularisation/index.blade.php
+++ resources/views/regularisation/index.blade.php
@@ -31,9 +31,7 @@
 			@include('regularisation.previous_loan_details',['member'=>$member])
 		@endif
 	@endif
-
     @include('regularisation.ordinary_loan_form')
-
 	@include('regularisation.caution_form')
 	
 	<?php $wording = isset($wording) ? $wording : trans('loan.regulating_loan_to',['loantype'=>$loanInputs['operation_type'],'names'=>$member->names]) ?>
