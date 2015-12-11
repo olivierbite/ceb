7c29e866cbc8d03a041bf66d8d6b351e6954a612
Kamaro Lambert <kamaroly@gmail.com>
2015-12-11 17:40:37 +0200

    Formatted numbers in illegularities reports

--- resources/views/reports/member/item_member_not_contributed.blade.php
+++ resources/views/reports/member/item_member_not_contributed.blade.php
@@ -2,7 +2,7 @@
  	<td>{{ $member->adhersion_id }}</td>
  	<td>{{ $member->first_name }} {{ $member->last_name }}</td>
 	<td>{{ $member->service }}</td>
-	<td>{{ $member->last_date }}</td>
-	<td>{{ $member->contributed_amount }}</td>
-	<td>{{ $member->to_pay }}</td>
+	<td>{{ date('Y-m-d', strtotime($member->last_date)) }}</td>
+	<td>{{ number_format((int)$member->contributed_amount) }}</td>
+	<td>{{ number_format((int) $member->to_pay) }}</td>
 </tr>
--- resources/views/reports/member/item_refund_irregularities.blade.php
+++ resources/views/reports/member/item_refund_irregularities.blade.php
@@ -2,12 +2,12 @@
  	<td>{{ $member->adhersion_id }}</td>
  	<td>{{ $member->first_name }} {{ $member->last_name }}</td>
 	<td>{{ $member->service }}</td>
-	<td>{{ $member->contributed_amount }}</td>
-	<td>{{ $loanBalance = $member->loansAmount - $member->refundedAmount }}</td>
+	<td>{{ number_format((int) $member->contributed_amount) }}</td>
+	<td>{{ number_format((int) $loanBalance = $member->loansAmount - $member->refundedAmount) }}</td>
 	<td>{{ $member->contributed_amount - $loanBalance }}</td>
 	<td>{{ date('Y-m-d',strtotime($member->last_date)) }}</td>
 	<td>{{ $member->comment }}</td>
-	<td>{{ round($loanBalance/$member->monthly_fees) }}</td>
+	<td>{{ number_format((int)$loanBalance/$member->monthly_fees) }}</td>
 	<td> n/a </td>
 	<td> n/a </td>
 </tr>
