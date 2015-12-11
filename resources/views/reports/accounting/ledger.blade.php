5763d8155ac69e537cf34f3075a98dbac66b9119
Kamaro Lambert <kamaroly@gmail.com>
2015-12-11 17:12:06 +0200

    Added order by date on the loan records

--- resources/views/reports/accounting/ledger.blade.php
+++ resources/views/reports/accounting/ledger.blade.php
@@ -14,16 +14,16 @@
 	<tr>
 		<td>{!! $posting->created_at->format('Y-m-d') !!}</td>
 		<td>{!! $posting->wording !!}</td>
-		<td>{!! $posting->debit_amount !!}</td>
-		<td>{!! $posting->credit_amount !!}</td>
+		<td>{!! abs($posting->debit_amount) !!}</td>
+		<td>{!! abs($posting->credit_amount) !!}</td>
 	</tr>
 	@empty
 		{{-- empty expr --}}
 	@endforelse
 	<tr>
 			<td colspan="2"><b>{{ trans('posting.wording') }}:</b> {!! $postings->first()->wording !!}</td>
-			<td>{!! $postings->sum('amount') !!}</td>
-			<td>{!! $postings->sum('amount') !!}</td>
+			<td>{!! abs($postings->sum('amount')) !!}</td>
+			<td>{!! abs($postings->sum('amount')) !!}</td>
 		</tr>
 	</tbody>
 </table>
