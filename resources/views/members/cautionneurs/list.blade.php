e9e060757960d253776aa989af562eedb52e582a
Kamaro Lambert <kamaroly@gmail.com>
2015-12-14 10:12:17 +0200

    Refactored cautionneurs table under member profile

--- resources/views/members/cautionneurs/list.blade.php
+++ resources/views/members/cautionneurs/list.blade.php
@@ -1,15 +1,11 @@
-<table class="table table-bordered" style="background-color: #fff">
-  	 <thead>
-  	 <tr>
-  	 	<th>
-  	 		{!! $title !!}
-  	 	</th>
-  	 </tr>
+<table class="table table-bordered" style="background-color: #fff;width: 100%;">
+<caption style="width: 100%;background-color: #fff;text-align: center;font-weight: 700;">{!! $title !!}</caption>
+ <thead>
  	<tr>
       <th>{{ trans('member.adhersion_number') }}</th>
   	 		<th>{{ trans('member.names') }}</th>
   	 		<th>{{ trans('member.institution') }}</th>
-  	 		<th>{{ trans('member.service') }}</th>
+  	 		{{-- <th>{{ trans('member.service') }}</th> --}}
 	        <th>{{ trans('member.amount') }}</th>
 	        <th>{{ trans('member.refunded_amount') }}</th>
 	        <th>{{ trans('member.balance') }}</th>
@@ -21,5 +17,12 @@
   @empty
     @include('members.no-items')
   @endforelse
+  <tr>
+  <th colspan="3">{{ trans('general.summary') }}</th>
+  
+  <th>{!! number_format(abs($cautions->sum('amount'))) !!}</th>
+  <th>{!! number_format(abs($cautions->sum('refunded_amount')))!!}</th>
+  <th>{!! number_format($cautions->sum('amount') - $cautions->sum('refunded_amount'))  !!}</th>
+</tr>
  </tbody>
 </table>
