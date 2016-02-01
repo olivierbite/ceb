2b0bdb76dc3392e4f9e4895b5a78f2375785d62f
Kamaro Lambert <kamaroly@gmail.com>
2016-02-01 09:34:42 +0200

    Added arrow html entity in the title

--- resources/views/reports/accounting/bilan.blade.php
+++ resources/views/reports/accounting/bilan.blade.php
@@ -1,6 +1,6 @@
 <table class="pure-table pure-table-bordered">
 
-<caption style="font-size: 16px;"> {{ trans('report.bilan') }} {{ Request::segment(4).' -->  '.Request::segment(5) }} </caption>
+<caption style="font-size: 16px;"> {{ trans('report.bilan') }} {{ Request::segment(4).' &#8596;  '.Request::segment(5) }} </caption>
 	<tr>
 		<td Valign="TOP">ACTIF_TABLE</td>
 		<td Valign="TOP">PASSIF_TABLE</td>
