a6808378959a4dd8bf85fa0d31f4efefbbf88bae
Kamaro Lambert <kamaroly@gmail.com>
2016-05-11 08:15:53 +0200

    Refactor loan monthly fees calculation and only consider latest loan

--- app/Models/User.php
+++ app/Models/User.php
@@ -612,33 +612,33 @@ class User extends SentinelModel {
 					$monthly_fee = $latest->monthly_fees;
 				}
 
-				// If we have emergency loan, check if this emergency
-				// loan is not the one being returned
-			 	if ($this->has_active_emergency_loan) {
+			// 	// If we have emergency loan, check if this emergency
+			// 	// loan is not the one being returned
+			//  	if ($this->has_active_emergency_loan) {
 
-			 	// If this one is an emergency loan, then reset 
-			 	// monthly loan to zero.
-				if ($this->active_emergency_loan->id == $latest->id) {
-					$monthly_fee = 0;
-				}
-			}
-			// If this latest loan is not ordinary loan, then check if this member
-			// has taken an ordinary loan which is not paid yet and add monthly
-			// fees to the ordinary loan 
-			// try
-			// {
-				// Get latest loan  details
-				$latest_ordinary_loan = $this->latest_ordinary_loan;
+			//  	// If this one is an emergency loan, then reset 
+			//  	// monthly loan to zero.
+			// 	if ($this->active_emergency_loan->id == $latest->id) {
+			// 		return $mon;
+			// 	}
+			// }
+			// // If this latest loan is not ordinary loan, then check if this member
+			// // has taken an ordinary loan which is not paid yet and add monthly
+			// // fees to the ordinary loan 
+			// // try
+			// // {
+			// 	// Get latest loan  details
+			// 	$latest_ordinary_loan = $this->latest_ordinary_loan;
                 
-				// Check if we have latest active ordinary loan that is not yet paid
-				// We need to add previous monthly fees, since this loan is either
-                // social loan or special loan 
+			// 	// Check if we have latest active ordinary loan that is not yet paid
+			// 	// We need to add previous monthly fees, since this loan is either
+   //              // social loan or special loan 
                 
-                if ($latest_ordinary_loan->id != $latest->id && !$latest_ordinary_loan->isFullPaid()) {
+   //              if ($latest_ordinary_loan->id != $latest->id && !$latest_ordinary_loan->isFullPaid()) {
                 	
-                	$monthly_fee+=$latest_ordinary_loan->monthly_fees;
+   //              	$monthly_fee+=$latest_ordinary_loan->monthly_fees;
 
-                }
+   //              }
                
 			}
 			catch(\Exception $ex)
@@ -650,9 +650,9 @@ class User extends SentinelModel {
 			// }
 			
         
-   //      if ($this->remainingInstallment() < 2) {
-   //      	return $this->loan_balance;
-   //      }
+   // //      if ($this->remainingInstallment() < 2) {
+   // //      	return $this->loan_balance;
+   // //      }
 		return $monthly_fee;	
 	}
 
