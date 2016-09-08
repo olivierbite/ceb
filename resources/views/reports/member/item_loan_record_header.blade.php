8661c9c98fbf8bd1ad4ad129eb14f6b61f31b50c
Kamaro Lambert <kamaroly@gmail.com>
2016-09-08 20:16:10 +0200

    Added cautionneurs on loan table

--- resources/views/reports/member/item_loan_record_header.blade.php
+++ resources/views/reports/member/item_loan_record_header.blade.php
@@ -2,13 +2,12 @@
 	<td colspan="3" >
 		{!! trans('loan.contract_number') !!} : {!! $loan->loan_contract !!}
 	</td>
-	
-	<td colspan="2" >  	
-		{!! trans('loan.cautionneur1') !!} 
-		  : {!! (empty(\Ceb\Models\Loan::find($loan->id)->cautionneur1) == false )?\Ceb\Models\Loan::find($loan->id)->cautionneur1->cautionneur_adhresion_id:null !!}
+	<?php $counter  = 1 ;?>
+	<?php $loan = \Ceb\Models\Loan::find($loan->loan_id);?>
+	@foreach ($loan->cautions as $caution)
+	<td colspan="{!! $counter == 1 ? '2': '4' !!}" >  	
+		{!! trans('loan.cautionneur'.$counter) !!} 
+		  :{!! $caution->cauttionneur->names !!} ({!! $caution->cautionneur_adhresion_id !!})
 	</td>
-    <td colspan="4" >   	
-		{!! trans('loan.cautionneur2') !!} : 
-		{!! (empty(\Ceb\Models\Loan::find($loan->id)->cautionneur2)==false )?\Ceb\Models\Loan::find($loan->id)->cautionneur2->cautionneur_adhresion_id:null;  !!}
-	</td>
+	@endforeach
 </tr>
